5ec4e6df5c908577fd2730d9698b00216a951b77
José Dotta <josehenriquedotta61@gmail.com>
2026-08-17 00:07:41 -0300

    chore(services): decommission obsolete direct bank integrations (Itau, MercadoPago, Inter)

--- tests/FinanceHub.UnitTests/Api/HealthEndpointsTests.cs
+++ tests/FinanceHub.UnitTests/Api/HealthEndpointsTests.cs
@@ -55,49 +55,7 @@ public class HealthEndpointsTests
         }
     }
 
-    [Fact]
-    public async Task ItauIntegration_GET_Health_ShouldReturnHealthyStatus()
-    {
-        using var factory = new CustomWebApplicationFactory<FinanceHub.ItauIntegration.Api.Program>();
-        await factory.InitializeAsync();
-        var client = factory.CreateClient();
 
-        try
-        {
-            var response = await client.GetAsync("/health");
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            var content = await response.Content.ReadFromJsonAsync<HealthResponse>();
-            content.Should().NotBeNull();
-            content!.Status.Should().Be("Healthy");
-            content.Service.Should().Be("FinanceHub.ItauIntegration.Api");
-        }
-        finally
-        {
-            await factory.DisposeAsync();
-        }
-    }
-
-    [Fact]
-    public async Task MercadoPagoIntegration_GET_Health_ShouldReturnHealthyStatus()
-    {
-        using var factory = new CustomWebApplicationFactory<FinanceHub.MercadoPagoIntegration.Api.Program>();
-        await factory.InitializeAsync();
-        var client = factory.CreateClient();
-
-        try
-        {
-            var response = await client.GetAsync("/health");
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            var content = await response.Content.ReadFromJsonAsync<HealthResponse>();
-            content.Should().NotBeNull();
-            content!.Status.Should().Be("Healthy");
-            content.Service.Should().Be("FinanceHub.MercadoPagoIntegration.Api");
-        }
-        finally
-        {
-            await factory.DisposeAsync();
-        }
-    }
 
     [Fact]
     public async Task TransactionAggregator_GET_Health_ShouldReturnHealthyStatus()
